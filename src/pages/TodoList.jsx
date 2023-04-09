2da362c37bb44aceb4665e79a9b2de36cede8b9a
juhee067 <66045537+juhee067@users.noreply.github.com>
2023-04-09 17:27:40 +0900

    feat:todolist에todo 추가하기

--- src/pages/TodoList.jsx
+++ src/pages/TodoList.jsx
@@ -1,28 +1,88 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./todolist.scss";
+import { useNavigate } from "react-router-dom";
+import { getAccessToken } from "../modules/Auth";
+import axios from "axios";
+
 const TodoList = () => {
+  const navigate = useNavigate();
+  let access_token = getAccessToken();
+
+  // input value
+  let [inputValue, setInputValue] = useState("");
+  // todoList 아이템
+  let [todoItems, setTodoItems] = useState([]);
+
+  // input 작성
+  const onTodoHandler = (e) => {
+    setInputValue(e.currentTarget.value);
+  };
+
+  // 추가
+  const onTodoAdd = async () => {
+    if (!inputValue) return;
+    const body = {
+      todo: inputValue,
+    };
+
+    try {
+      let response = await axios.post(
+        "https://www.pre-onboarding-selection-task.shop/todos",
+        body,
+        {
+          headers: {
+            Authorization: `Bearer ${access_token}`,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      setTodoItems((prevList) => [...prevList, { ...response.data }]);
+      setInputValue("");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  // token redirection
+  useEffect(() => {
+    const token = getAccessToken();
+    if (!token) {
+      navigate("/signin");
+    }
+  }, [navigate]);
   return (
     <div className="todo">
       <div className="add">
         <h1>Todo List</h1>
-        <input data-testid="new-todo-input" />
-        <button data-testid="new-todo-add-button">추가</button>
+        <input
+          data-testid="new-todo-input"
+          onChange={onTodoHandler}
+          value={inputValue}
+        />
+
+        <button data-testid="new-todo-add-button" onClick={onTodoAdd}>
+          추가
+        </button>
       </div>
       <ul>
         <li className="title">
           <span>완료</span>
           <span>해야 할 일</span>
         </li>
-        <li>
-          <label>
-            <input type="checkbox" className="checkBox" />
-            <span>아침 먹고 운동가기</span>
-            <div className="edit">
-              <button data-testid="modify-button">수정</button>
-              <button data-testid="delete-button">삭제</button>
-            </div>
-          </label>
-        </li>
+        {todoItems.map((todoItem) => {
+          return (
+            <li key={todoItem.id}>
+              <label>
+                <input type="checkbox" className="checkBox" />
+                <span>{todoItem.todo}</span>
+                <div className="edit">
+                  <button data-testid="modify-button">수정</button>
+                  <button data-testid="delete-button">삭제</button>
+                </div>
+              </label>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
