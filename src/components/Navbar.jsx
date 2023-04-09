da2d68454ad9fe18264717c1b669b2dba540f143
juhee067 <66045537+juhee067@users.noreply.github.com>
2023-04-09 14:43:49 +0900

    feat:logout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./navbar.scss";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { getAccessToken, removeAccessToken } from "../modules/Auth";
 const Navbar = () => {
+  const navigate = useNavigate();
+  const access_token = getAccessToken();
+  const [token, setToken] = useState(access_token);
+  const logoutHandler = () => {
+    removeAccessToken();
+    localStorage.removeItem("todoList");
+    setToken(null);
+  };
+
+  const loginHandler = () => {
+    setToken(null);
+  };
+
+  useEffect(() => {
+    setToken(getAccessToken());
+  }, [navigate]);
+
   return (
     <div>
       {" "}
       <ul>
         <li>
-          <Link className="link" to={"/signin"}>
-            로그인
-          </Link>
+          {token ? (
+            <Link className="link" to={"/signin"} onClick={logoutHandler}>
+              로그아웃
+            </Link>
+          ) : (
+            <li>
+              <Link className="link" to={"/signin"} onClick={loginHandler}>
+                로그인
+              </Link>
+            </li>
+          )}
         </li>
         <li>
           <Link className="link" to={"/signup"}>
